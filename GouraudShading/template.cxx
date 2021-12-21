41417c1a590283d21a52caba8239a8e5ed3db1bc
Blane Staskiewicz <bas6248@psu.edu>
2021-12-21 14:25:51 -0500

    Update template.cxx

--- GouraudShading/template.cxx
+++ GouraudShading/template.cxx
@@ -1,68 +1,16 @@
-/*********************************************************************
- *  CMPSC 457 Section 001                                            *
- *  Homework 3                                                       *
- *                                                                   *
- *  Blane Staskiewicz                                                *
- *  bas6248@psu.edu - 919215349                                      *
- *                                                                   *
- *  10/13/21                                                         *
- *********************************************************************/  
-
-
-
-
-/*********************************************************************
- *  CMPSC 457                                                        *
- *  Template code for HW 3                                           *
- *                                                                   *
- *  Sukmoon Chang                                                    *
- *  sukmoon@psu.edu                                                  *
- *                                                                   *
- *                                                                   *
- *                                                                   *
- *  Description:                                                     *
- *                                                                   *
- *  This is a template code for homework 3.                          *
- *  It takes three points from users through left button click.      *
- *  Then, it draws a triangle with the three points as its vertices. *
- *                                                                   *
- *                                                                   *
- *  User interface:                                                  *
- *                                                                   *
- *  1. When it starts, its shading mode is set to WIREFRAME.         *
- *     WIREFRAME mode is already implemented for your reference.     *        
- *  2. For flat shading, press 'f' to put it in FLAT shading mode.   *
- *     Then, select 3 points by clicking left mouse button.          *
- *     The program draws a triangle with the selected points         *
- *     as its vertices using the color of your choice.               *
- *  3. For Gouraud shading, press 'g' to put it in Gouraud           *
- *     shading mode. Then, select 3 points by clicking left mouse    *
- *     button. The program draws a triangle with the selected points *
- *     as its vertices, interpolating the vertex colors (red, green, *
- *     blue for the first, second, third points in that order)       *
- *  4. Press 'w' to go back to WIREFRAME mode.                       *
- *  5. To quit the program, press 'q'.                               *
- *  6. Any other keys that are not used to switch shading modes      *
- *     will put the shading mode to WIREFRAME.                       *
- *********************************************************************/  
- 
-
 #include <GL/glut.h>
 #include <stdlib.h>
 #include <iostream>
 
-
 using std::cin;
 using std::cerr;
 using std::endl;
-
 
 // callbacks for glut (see main() for what they do)
 void reshape(int w, int h);
 void display();
 void mouse(int button, int state, int x, int y);
 void keyboard(unsigned char key, int x, int y); 
-
 
 // Simple structure for a point
 struct Point
@@ -83,46 +31,29 @@ struct Color
     Color(float r, float g, float b) : r(r), g(g), b(b) {}
 };
 
-
 // helpers
 void init();
 void addPoint(int x, int y);
 void keyboard_input();
 void draw_point(int x, int y, Color c);
 void draw_line(int x0, int y0, int x1, int y1, Color c);
-
-
 void draw_triangle();
 void triangle_wireframe(Color color);
 
-
-// Keeps track of current shading mode.
+// Keeps track of current shading mode
 enum ShadingMode { WIREFRAME, FLAT, GOURAUD };
 ShadingMode shading_mode = WIREFRAME;
-
-
 
 // Initial window size
 int win_w = 512;
 int win_h = 512;
 
-
-// For triangles, 3 points will do.
+// For triangles, 3 points will do
 Point points[3];
 
 // Used to keep track of how many points I have so far
 int num_points;
 
-
-
-// OpenGL/glut programs typically have the structure shown here,
-// although with different args and callbacks.
-//
-// You should not need to modify main().
-// If you want to modify it, do it at your own risk.
-//
-// For complete description of each glut functions used, see
-// glut manual page.
 int main(int argc, char* argv[])
 {
     // initialize glut
@@ -139,32 +70,21 @@ int main(int argc, char* argv[])
     // now, create window with title "Scan Conversion"
     glutCreateWindow("Scan Conversion");
 
-
     // other stuffs like background color, viewing, etc will be
     // set up in this function.
     init();
 
-  
     // register callbacks for glut
     glutDisplayFunc(display);   // for display
     glutReshapeFunc(reshape);   // for window move/resize
     glutMouseFunc(mouse);       // for mouse buttons
     glutKeyboardFunc(keyboard); // for keyboard
 
-
     // start event processing, i.e., accept user inputs
     glutMainLoop();
 
     return 0;
 }
-
-
-
-
-/*****************
- *   callbacks   *
- *****************/
-
 
 // called when the window is resized/moved (plus some other cases)
 void reshape(int w, int h)
@@ -182,32 +102,14 @@ void reshape(int w, int h)
 
 // called when the window needs to be redrawn
 void display()
-{
-    // buffer is a raster array provided by OpenGL
-    // recall that we are using 2 buffers (double buffering)
-    // they are called front buffer and back buffer
-    // what you see on the screen is the content of front buffer
-    // what you draw is drawn only on back buffer
-
-    
+{    
     // clear back buffer with background color that is set in init()
     glClear(GL_COLOR_BUFFER_BIT);
 
     // now, draw on back buffer just cleared
     draw_triangle();
-
-    // swap the buffers.
-    // - all the drawing is done on the back buffer
-    // - once drawing is done on the back buffer, 
-    //       you need to display the content of the back buffer.
-    // - swapping buffers means swapping back buffer with front buffer
-    //       so that front buffer becomes back buffer and
-    //       back buffer becomes front buffer.
-    // - once back buffer becomes front buffer, the content of it will be
-    //       displayed on the screen, so you can see it.
     glutSwapBuffers();
 }
-
 
 // called when a mouse event (button pressed/released/moved/dragged) occurs
 // in glut, 
@@ -228,7 +130,6 @@ void mouse(int button, int state, int x, int y)
     }
 }
 
-
 // called when a keyboard event (key typed) occurs
 void keyboard(unsigned char key, int x, int y)
 {
@@ -241,7 +142,7 @@ void keyboard(unsigned char key, int x, int y)
     case 'g':  // gouraud shading
 	shading_mode = GOURAUD;
 	break;
-    case 'k':  // for grading purpose only--do not modify
+    case 'k':
 	keyboard_input();
 	num_points = 0;
 	break;
@@ -250,14 +151,6 @@ void keyboard(unsigned char key, int x, int y)
 	break;
     }
 }
-
-
-
-
-/**************
- *   helpers  *
- **************/
-
 
 void init()
 {
@@ -269,38 +162,22 @@ void init()
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(0.0, win_w-1, 0.0, win_h-1, -1.0, 1.0);
-
 }
-
-
 
 // add the point just selected by mouse button
 void addPoint(int x, int y)
 {
     points[num_points++] = Point(x, y);
     if (num_points == 3) {
-	// we have 3 points now, so we can draw a triangle
-	    
 	// reset the num_points to 0 for next line
 	num_points = 0;
 
-	// tell glut that the current window needs to be redisplayed.
-	// glut will then redisplay the current window.
-	// this means display() callback will be called.
-	// display() in turn will draw a triangle on back buffer
-	//   and swap the back buffer with the front buffer
-	// by swapping the buffers, the back buffer becomes visible,
-	//   ie, displayed on the window
+	// tell glut that the current window needs to be redisplayed
+	// glut will then redisplay the current window
 	glutPostRedisplay();
     }
 }
 
-
-
-// for grading purpose only
-// do not modify this function
-// do not use this function
-//   -- it's not tested fully and does not work correctly
 void keyboard_input()
 {
     int x, y;
@@ -313,7 +190,6 @@ void keyboard_input()
     }
 }
  
-
 void draw_point(int x, int y, Color c)
 {
     glBegin(GL_POINTS);
@@ -323,7 +199,6 @@ void draw_point(int x, int y, Color c)
     }
     glEnd();
 }
-
 
 void draw_line(int x0, int y0, int x1, int y1, Color c)
 {
@@ -339,10 +214,6 @@ float area(int x1, int y1, int x2, int y2, int x3, int y3)
    return abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
 }
 
-// a helper function to draw a triangle
-// WIREFRAME mode is implemented for your reference
-// you need to modify this function for other drawing modes
-// anything you draw here goes to back buffer
 void draw_triangle()
 {
     switch (shading_mode) {
@@ -410,7 +281,6 @@ void draw_triangle()
             		} 
         	} 
     	}
-
 	break;
     }
     case GOURAUD:
@@ -477,12 +347,10 @@ void draw_triangle()
                         }
                 }
         }
-        
 	break;
     }
     }
 }
-
 
 void triangle_wireframe(Color color)
 {
