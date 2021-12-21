70109b1dc38796e14e322ba8f4bfb4c2912762ab
Blane Staskiewicz <bas6248@psu.edu>
2021-12-21 13:36:00 -0500

    Update template.cxx

--- Raying_Tracing/template.cxx
+++ Raying_Tracing/template.cxx
@@ -7,12 +7,9 @@
 #include <vector>
 #include <glm/glm.hpp>
 
-
 using namespace std;
 
 using vec3 = glm::dvec3;
-
-
 
 struct Ray
 {
@@ -23,36 +20,28 @@ struct Ray
     Ray (vec3 o, vec3 d) : o(o), d(d) {} 
 };
 
-
-
 // types of the surface
 // - DIFFuse, SPECular, REREective
-// - only DIFF is used in this assignment
 enum Refl_t { DIFF, SPEC, REFR };
 
 // small constant
 const double eps = 1e-4;
 
-
 struct Sphere
 {
-    double r;      // radius of sphere
-    vec3 p;        // position (center) of sphere
-    vec3 e;        // emission (not used in this assignment)
-    vec3 c;        // color of sphere
+    double r;      // radius
+    vec3 p;        // position (center)
+    vec3 e;        // emission
+    vec3 c;        // color
     Refl_t refl;   // reflection type (DIFFuse, SPECular, REFRactive)
-                   // - in this assignment, only DIFF is used
 
     Sphere(double r, vec3 p, vec3 e, vec3 c, Refl_t refl)
         : r{r}, p{p}, e{e}, c{c}, refl{refl} {}
 
     double intersect(const Ray& ray) const
     {
-	// Does the ray intersect this sphere?
-	// - if it does with t > eps, return t
-	// - otherwise, return 0
-	//
-        // Need to solve (d.d)t^2 + 2d.(o-c)t + (o-c).(o-c) - R^2 = 0
+	// intersects with t > eps, return t
+	// otherwise, return 0
 	    
 	vec3 op = ray.o - p;
 	double b = glm::dot(op, 2.0 * ray.d);
@@ -71,43 +60,21 @@ struct Sphere
 
     vec3 normal(vec3& v)
     {
-	// v is a hit point on the sphere
-	// return the unit normal at the hit point
-	//
-	// you may want to use glm::normalize()
-	//    which takes a vector and returns a unit vector
-
 	return glm::normalize(v - p);
     }
 };
 
-
-// Three spheres in the scene
 vector<Sphere> spheres = {
     Sphere(200, vec3(  0, -300, -1200), vec3(), vec3(.8, .8, .8), DIFF),
     Sphere(200, vec3(-80, -150, -1200), vec3(), vec3(.7, .7, .7), DIFF),
     Sphere(200, vec3( 70, -100, -1200), vec3(), vec3(.9, .9, .9), DIFF)
 };
 
-
 vec3 eye(0, 0, 200);      // camera position
 vec3 light(0, 0, 200);    // light source position
 
-
-
-// Does this ray hit any of the spheres?
-// if so,
-// - return t using the reference parameter
-// - return the index of the surface using the reference parameter
-// - return true
-// if not,
-// - return false
 bool hit(const Ray& ray, double& t, int& surface_idx)
 {
-    // You must check if this ray intersect with each of the spheres
-    // and keep track of the closest t and the index of the corresponding sphere
-    // Return the closest t and the index using the reference arguments
-
     double valt = INT_MAX;
     bool hit = false;
     for (int i = 0; i < 3; i++) {
@@ -123,10 +90,7 @@ bool hit(const Ray& ray, double& t, int& surface_idx)
     return hit;
 }
 
-
-// Calculate the intensity using Lambert's law
-// ie, dot product of l_hat and n_hat (or 0 if dot product is negative)
-// Note that n_hat is a unit normal vector at the point where the ray hits the surface
+// Calculating the intensity using Lambert's law
 double lambert(int surface_idx, Ray& ray, double t)
 {
     vec3 Pn = eye + (t * ray.d);
@@ -138,19 +102,12 @@ double lambert(int surface_idx, Ray& ray, double t)
 }
 
 
-// Calculate the color of the ray
-// If ray hits any surface,
-// - color of the ray is the Lambert's intensity * color at the hit point
-// If not
-// - color of the ray is the background color (black)
+// Calculating the color of the ray
 vec3 ray_color(Ray& ray)
 {
     double t;
     int surface_idx;
 
-    // If is_hit is true (i.e., the ray hit at least one of the spheres)
-    // then, t is the t for the closest sphere hit by the ray
-    //       surface_idx is the index of the corresponding sphere
     bool is_hit = hit(ray, t, surface_idx);
     if (is_hit) {
         return spheres[surface_idx].c * lambert(surface_idx, ray, t);
@@ -160,31 +117,19 @@ vec3 ray_color(Ray& ray)
     }
 }
 
-
 // Simple ray tracer
 void tracer(int nx, int ny, int d, double theta, ofstream& fout)
 {
-    // Generate a ppm image of size nx x ny 
-    //
-    // 1. calculate h, w, scale_factor based on nx, ny, d, and theta
-    // 2. for every pixel p' in nx x ny raster
-    // 3.    transform p' = (x', y') in pixel coord to p = (x, y) in world coord
-    // 4.    define a ray passing through the pixel
-    //           (note: direction vector of the ray must be a unit vector)
-    // 5.    calculate the color of the ray, ie, by calling ray_color()
-    // 6.    use the color for pixel p'
-    // 7. end for
- 
     double h = (2.0 * d * (tan(theta / 2.0)));
     double w =  (nx / (double) ny) * h;
     double scalef = w / (double) nx;
 
     fout << "P3\n" << nx << " " << ny << "\n" << "255\n";
 
-    // iterate color values into raster
     for (int j = 0; j < ny; j++) {
         for (int i = 0; i < nx; i++) {
             vec3 point;
+		
 	    // transform: translate, scale
             point[0] = ((double) i + (-nx / 2.0)) * scalef;
             point[1] = ((double) j + (-ny / 2.0)) * scalef;
@@ -193,6 +138,7 @@ void tracer(int nx, int ny, int d, double theta, ofstream& fout)
             vec3 worldn = glm::normalize(point - eye);
             Ray r = Ray(eye, worldn);
             vec3 color = ray_color(r);
+		
             fout << (int) (color[0] * 255) << " " << (int) (color[1] * 255) << " " << (int) (color[2] * 255) << " ";
 
       }
@@ -222,12 +168,7 @@ int main(int argc, char* argv[])
     //   the virtual film placed at the distance of 200 in z-axis (negative z direction) from the eye
     //   vfov of 120
     tracer(nx, ny, 200, 120, fout);
-
     fout.close();
 
     return 0;
 }
-
-
-
-
